b9d91b7ddfa4cf712e3675edd24a428569fd3119
brentlaster <Brent.Laster@sas.com>
2021-07-11 11:34:57 -0400

    update startup command

--- extra/start-kube.sh
+++ extra/start-kube.sh
@@ -1,1 +1,5 @@
-sudo minikube start --extra-config=apiserver.GenericServerRunOptions.AdmissionControl=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ResourceQuota,DefaultTolerationSeconds,PodSecurityPolicy 
+sudo minikube delete --all --purge
+sudo minikube start --vm-driver=none --kubernetes-version=v1.20.7  --extra-config=apiserver.enable-admission-plugins=PodSecurityPolicy,NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ResourceQuota,DefaultTolerationSeconds --addons=pod-security-policy
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+chmod -R u+wrx $HOME/.minikube
+
