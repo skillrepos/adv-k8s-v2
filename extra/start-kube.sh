0fea320b31dde400298cfcfbd48cd0d1fed7c189
brentlaster <Brent.Laster@sas.com>
2022-10-11 09:07:06 -0400

    Update script to newer K8s

--- extra/start-kube.sh
+++ extra/start-kube.sh
@@ -1,4 +1,4 @@
-sudo minikube start --vm-driver=none --kubernetes-version=v1.21.11  --extra-config=apiserver.enable-admission-plugins=PodSecurityPolicy,NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ResourceQuota,DefaultTolerationSeconds --addons=pod-security-policy
+sudo minikube start --vm-driver=none --kubernetes-version=v1.23.1  --extra-config=apiserver.enable-admission-plugins=PodSecurityPolicy,NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ResourceQuota,DefaultTolerationSeconds --addons=pod-security-policy
 sudo chown -R $USER $HOME/.kube $HOME/.minikube
 chmod -R u+wrx $HOME/.minikube
 
